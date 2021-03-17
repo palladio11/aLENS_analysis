7c9b63d333fa6121c2eed566e83a5f6bbf660345
wenyan4work <wenyan4work@gmail.com>
2021-03-17 11:38:37 -0400

    add gr_sq

--- Gr_Sq.py
+++ Gr_Sq.py
@@ -2,13 +2,13 @@ import numpy as np
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import scipy.special as ss
-import PointCloud as pc
 import h5py
 
 import os
-import glob
 import argparse
-import re
+
+import AMSOS as am
+import point_cloud.PointCloud as pc
 
 boxsize = np.array([10, 10, 10])
 
@@ -23,32 +23,6 @@ parser.add_argument('--rcut', type=float, dest='rcut', default=1.5,
 args = parser.parse_args()
 # meanWindow = args.window
 print('rcut: ', args.rcut)
-
-
-def getFrameNumber_lambda(filename): return int(
-    re.search('_([^_.]+)(?:\.[^_]*)?$', filename).group(1))
-
-
-class Tubule:
-    def __init__(self, linestring):
-        data = linestring.split()
-        self.gid = int(data[1])
-        self.minus = np.array([float(data[2]), float(data[3]), float(data[4])])
-        self.plus = np.array([float(data[5]), float(data[6]), float(data[7])])
-        xi = (self.plus-self.minus)
-        self.orientation = xi/np.sqrt(xi.dot(xi))
-
-
-class Frame:
-    def __init__(self, filename):
-        self.TList = np.loadtxt(filename,
-                                skiprows=2, usecols=(1, 2, 3, 4, 5, 6, 7, 8))
-        self.filename = filename
-        file = open(filename, 'r')
-
-
-def get_basename(filename):
-    return os.path.splitext(os.path.basename(filename))[0]
 
 
 def calc_gr_sq(points, foldername, filename):
@@ -85,7 +59,7 @@ def calc_gr_sq(points, foldername, filename):
     ax2.set_xlim(0, np.max(q))
     # ax2.set_ylim(0.5, 50)
     plt.tight_layout()
-    name = get_basename(filename)
+    name = am.get_basename(filename)
     plt.savefig(foldername+os.path.sep+name+'.png', dpi=300)
     plt.close(fig)
     plt.clf()
@@ -115,30 +89,27 @@ def process_frame(frame):
 
     r, rdf, q, Sq = calc_gr_sq(minus_pts, 'rdf_sq_minus', frame.filename)
     data_gr_sq = h5py.File('gr_sq.hdf5', 'a')
-    grp = data_gr_sq.create_group(get_basename(frame.filename))
+    grp = data_gr_sq.create_group(am.get_basename(frame.filename))
     save_data(grp, 'minus', r, rdf, q, Sq)
     data_gr_sq.close()
 
     r, rdf, q, Sq = calc_gr_sq(plus_pts, 'rdf_sq_plus', frame.filename)
     data_gr_sq = h5py.File('gr_sq.hdf5', 'a')
-    grp = data_gr_sq.require_group(get_basename(frame.filename))
+    grp = data_gr_sq.require_group(am.get_basename(frame.filename))
     save_data(grp, 'plus', r, rdf, q, Sq)
     data_gr_sq.close()
 
     r, rdf, q, Sq = calc_gr_sq(center_pts, 'rdf_sq_center', frame.filename)
     data_gr_sq = h5py.File('gr_sq.hdf5', 'a')
-    grp = data_gr_sq.require_group(get_basename(frame.filename))
+    grp = data_gr_sq.require_group(am.get_basename(frame.filename))
     save_data(grp, 'center', r, rdf, q, Sq)
     data_gr_sq.close()
 
     return
 
 
-files = glob.glob('result*-*'+os.path.sep+'SylinderAscii_*.dat')
-# sort as numerical order
-files.sort(key=getFrameNumber_lambda)
-print(files)
+files = am.getFileListSorted('result*-*/SylinderAscii_*.dat')
 
 for f in files[-10:]:
-    frame = Frame(f)
+    frame = am.FrameAscii(f, readProtein=False, info=False)
     process_frame(frame)
