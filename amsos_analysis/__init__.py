ca70b4aaf825485191af8e0547fccfa86222e236
Adam Lamson <lamsoa729@gmail.com>
2021-06-14 16:30:48 -0400

    Added to __init__.py

--- amsos_analysis/__init__.py
+++ amsos_analysis/__init__.py
@@ -5,3 +5,11 @@
 __author__ = """Adam Reay Lamson"""
 __email__ = 'alamson@flatironinstitute.org'
 __version__ = '0.1.0'
+
+from .chrom_analysis import (gauss_weighted_contact,
+                             log_gauss_weighted_contact,
+                             get_link_energy_arrays,
+                             get_sep_hist,
+                             get_sep_dist_mat,
+                             get_overlap_arrs,
+                             )
