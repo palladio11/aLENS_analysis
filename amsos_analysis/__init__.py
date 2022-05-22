77f4e1f6d8244489239b823b0fc73de66635531f
Adam Lamson <alamson@flatironinstitute.org>
2022-05-22 12:20:09 -0400

    Minor changes to clean up last merge

--- amsos_analysis/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-"""Top-level package for AMSOS analysis."""
-
-__author__ = """Adam Reay Lamson, Wen Yan"""
-__email__ = 'alamson@flatironinstitute.org, wyan@flatironinstitute.org'
-__version__ = '0.1.0'
