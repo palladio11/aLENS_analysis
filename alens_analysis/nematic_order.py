2daef34c8f262904ab3aa6034ed9a7dcf75dd90f
Adam Lamson <lamsoa729@gmail.com>
2024-02-22 10:58:47 -0500

    Updated nematic order code

--- alens_analysis/nematic_order.py
+++ alens_analysis/nematic_order.py
@@ -7,6 +7,8 @@ Email: alamson@flatironinstitute.org
 Description:
 """
 import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
 
 
 def calc_nematic_order(syls):
@@ -37,8 +39,22 @@ def calc_nematic_order(syls):
 
     return np.array(nematic_order)
 
+def make_nematic_tensor_arr(direct_arr):
+    """Make the nematic tensor from a list of directors
+    N x 3 array of directors
+
+    :directors: List of directors
+    :returns: Nematic tensor
+
+    """
+    lengths = np.linalg.norm(direct_arr, axis=1)
+    unit_dirs = direct_arr / lengths[:, None]
+    nematic_tensor_arr = (np.einsum('ij,il->ijl', unit_dirs, unit_dirs) 
+                      - np.eye(3)[:,:]/3.)
+    return nematic_tensor_arr
+
 def nematic_analysis(direct_arr):
-    """Takes in a list of orientations and calculates the nematic order parameter and director
+    """Takes in a list of orientations and calculates the average nematic order parameter and director
 
     Parameters
     ----------
@@ -57,4 +73,29 @@ def nematic_analysis(direct_arr):
     nem_director = eigvecs[:, sort_inds[-1]]
     return nem_order, nem_director
 
+def fourier_transform_nematic_tensor_arr(nematic_tensor_arr, r_arr, k=[0.,0.,1.]):
+    """Take the fourier transform of the nematic tensor array
 
+    :nematic_tensor_arr: N x 3 x N array of nematic tensors
+    :returns: Fourier transform of the nematic tensor array
+
+    """
+    kr = np.einsum('ni,i->n', r_arr, k)
+    fQ = nematic_tensor_arr * (np.cos(kr) + 1j * np.sin(kr))[:, None, None]
+
+    return fQ
+
+def make_structure_factor(Q_arr, r_arr, k):
+    """Make the structure factor from the fourier transform of the nematic tensor array
+
+    :fQ: Fourier transform of the nematic tensor array
+    :returns: Structure factor
+
+    """
+
+    fQ_arr = fourier_transform_nematic_tensor_arr(Q_arr, r_arr, k=k)
+
+    # Find structure factor
+    Snm = np.einsum('nij,mij->nm', fQ_arr, fQ_arr.conj()) 
+    Snm -= np.diag((2./3.)*np.ones(Snm.shape[0]))
+    return np.mean(Snm).real
